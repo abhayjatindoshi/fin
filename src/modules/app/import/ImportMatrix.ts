be3f4683e11e621b37a5d41007b6be190fcde2f0
Abhay Jatin Doshi <abhaydoshi@microsoft.com>
2025-11-28 19:26:12 +0530

    fixes

--- src/modules/app/import/ImportMatrix.ts
+++ src/modules/app/import/ImportMatrix.ts
@@ -1,13 +1,29 @@
 import { FederalBankCreditCard } from "./adapters/federal/FederalBankCreditCard";
 import { HdfcBankAccount } from "./adapters/hdfc/HdfcBankAccount";
 import { JupiterBankAccount } from "./adapters/jupiter/JupiterBankAccount";
+import type { IEmailImportAdapter } from "./interfaces/IEmailImportAdapter";
+import type { IFileImportAdapter } from "./interfaces/IFileImportAdapter";
+import type { IImportAdapter } from "./interfaces/IImportAdapter";
 
 export class ImportMatrix {
-    static {
-        [
+
+    static adapters: Record<string, IImportAdapter> = {};
+    static fileAdapters: Record<string, IFileImportAdapter> = {};
+    static emailAdapters: Record<string, IEmailImportAdapter> = {};
+
+
+    static init() {
+        const adapters = [
             new HdfcBankAccount(),
-            new FederalBankCreditCard(),
             new JupiterBankAccount(),
+            new FederalBankCreditCard(),
         ]
+
+        adapters.forEach(adapter => {
+            ImportMatrix.adapters[adapter.name] = adapter;
+            if (adapter instanceof IFileImportAdapter) {
+                ImportMatrix.fileAdapters[adapter.name] = adapter;
+            }
+        });;
     }
 }
